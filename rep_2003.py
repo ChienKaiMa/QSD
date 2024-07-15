8ce9969a7f5854fd74569147ade503eb15a2492c
ChienKaiMa <koovakevy@gmail.com>
2024-07-15 15:45:09 +0800

    👍 Code refinements

--- rep_2003.py
+++ rep_2003.py
@@ -26,19 +26,17 @@ def reproduce_2003_Eldar_results():
         q.append(np.outer(recip_psi[i], recip_psi[i]).round(1))
     q = np.array(q)
 
-    I = np.identity(3)
+    I = np.identity(n)
     p = cp.Variable(n)
     objective = cp.Minimize(1 + cp.sum(prior_prob @ p))
     constraints = [
         0 <= p[0],
         0 <= p[1],
         0 <= p[2],
-        I - cp.multiply(p[0], q[0]) - cp.multiply(p[1], q[1]) - cp.multiply(p[2], q[2])
-        >> 0,  # Matrix inequality uses >>
+        I - p[0] * q[0] - p[1] * q[1] - p[2] * q[2] >> 0,  # Matrix inequality uses >>
     ]
 
     prob = cp.Problem(objective, constraints)
-
     result = prob.solve()
     print("Result =", result.round(3))
     # An acceptable optimal solution
@@ -48,6 +46,7 @@ def reproduce_2003_Eldar_results():
     print(pi1.round(5))
     # Wrong answer if we over postprocess the solution
     sol_overround = p.value.round(2)
+    print("Overprocessed solution =", sol_overround)
     pi1_overround = (
         I - sol_overround[0] * q[0] - sol_overround[1] * q[1] - sol_overround[2] * q[2]
     )  # Not positive semidefinite
