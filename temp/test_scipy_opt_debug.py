Add conj to fix the formula
scipy's null_space makes `dot(.) = 0`, but I wanted `vdot(.) = 0`.

temp/test_scipy_opt_debug.py
@@ -68,7 +68,7 @@ class NullSpaceSearchProblem:
                 if i != j:
                     A.append(self.states[i].data)
 
-            null_s = null_space(np.array(A))
+            null_s = null_space(np.conj(np.array(A)))
             null_spaces.append(null_s)
 
             for i in range(self.num_basis):