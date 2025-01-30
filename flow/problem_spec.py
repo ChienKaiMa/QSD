ead5f329b86414e1c2005d92a0c73df58bae8e73
ChienKaiMa <koovakevy@gmail.com>
2025-01-30 23:31:23 +0800

    Add misc code

--- flow/problem_spec.py
+++ flow/problem_spec.py
@@ -10,6 +10,7 @@ from temp.get_random_seeds import *
 from qiskit.quantum_info import (
     random_statevector,
     random_density_matrix,
+    DensityMatrix,
 )
 
 
@@ -110,4 +111,86 @@ class ProblemSpec:
             self.state_type = state_type
         return
 
-    pass
+    def depolarizing_noise_channel(num_qubits):
+        assert num_qubits > 0
+        return np.identity(2**num_qubits) / 2**num_qubits
+
+    def is_lin_indep(self):
+        """Check the vectors are linearly independent."""
+        l = []
+        for i in range(self.num_states):
+            l.append(self.states[i].data)
+        m = np.array(l)
+        rank = np.linalg.matrix_rank(m)
+        print("Rank =", rank)
+        print("#states =", self.num_states)
+
+    def gen_noisy_states(
+        num_qubits: int,
+        num_states: int,
+        seeds: list[int] = [],
+        noise_level: float = 0.01,
+        noise_rank: int = 2,
+        **kwargs,
+    ):
+        logger = logging.getLogger(__name__)
+        assert num_qubits > 0
+        assert num_states > 1
+        # TODO Sparse state generation
+        if len(seeds):
+            if len(seeds) != num_states:
+                logger.error(
+                    f"The number of provided seeds ({len(seeds)}) does not match the number of states ({num_states})"
+                )
+                return
+            if len(seeds) != len(set(seeds)):
+                logger.error(f"There are duplicate seeds")
+                return
+            logger.info("Use the seeds from the keyword arguments")
+        else:
+            logger.info(f"Use range({num_states}) as the seeds")
+            seeds = list(range(num_states))
+
+        # Initialize a set of pure states
+        states = [
+            random_statevector(
+                2**num_qubits,
+                seed=seeds[_],
+            )
+            for _ in range(num_states)
+        ]
+
+        # TODO Check orthogonality
+
+        # Turn the pure states to density matrices
+        dense_states = [DensityMatrix(states[i]) for i in range(num_states)]
+
+        # Create a different set of random density matrices
+        ## disturbance_states = [
+        ##     random_density_matrix(
+        ##         dims=2**num_qubits,
+        ##         rank=noise_rank,
+        ##         method="Hilbert-Schmidt",  # TODO method
+        ##         seed=seeds[_],
+        ##     )
+        ##     for _ in range(num_states)
+        ## ]
+
+        disturbance_states = [
+            DensityMatrix(ProblemSpec.depolarizing_noise_channel(num_qubits))
+            for _ in range(num_states)
+        ]
+        # Combine these
+
+        combined_states = [
+            (1 - noise_level) * dense_states[_].data
+            + noise_level * disturbance_states[_].data
+            for _ in range(num_states)
+        ]
+
+        combined_states = [DensityMatrix(combined_states[_]) for _ in range(num_states)]
+
+        logger.info(
+            f"{num_states} random {num_qubits}-qubit noisy states are generated"
+        )
+        return dense_states, disturbance_states, combined_states
