c318bff6ef5a0e20ecdba994d3c2c29486a2ed64
ChienKaiMa <koovakevy@gmail.com>
2025-10-31 15:45:03 +0800

    Add messages to the smoke test

--- quick_test_tacas.py
+++ quick_test_tacas.py
@@ -1,6 +1,7 @@
 from utils.handy_states import simple_2
 from flow.solve_mix import *
 
+print("Start the smoke test.")
 state_dict = simple_2(0.2, 0.5, 0.7)  # predefined quantum state set
 num_qubits = state_dict["num_qubits"]
 num_states = state_dict["num_states"]
@@ -14,6 +15,7 @@ qsd_problem.set_states(
     states=dense_mat,
     overwrite=True,
 )
+print("Successfully initialized a QSD ProblemSpec.")
 
 
 from flow.build_circuits import *
@@ -24,8 +26,11 @@ crossqsd_prob = apply_crossQSD(
     alpha=[0.02] * qsd_problem.num_states,
     beta=[0.02] * qsd_problem.num_states,
 )
+print("Successfully found a POVM solution.")
 
 povm_vectors = get_povm_vectors(crossqsd_prob)
 povm_ckt = POVMCircuit(povm_vectors=np.stack(povm_vectors))
 povm_ckt.fix()
 povm_ckt.build_circuit()
+print("Successfully built a quantum circuit for QSD!")
+print("Smoke test passed!")
